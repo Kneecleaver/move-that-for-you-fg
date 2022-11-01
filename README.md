3bfab896efe0fe083ae4eaf4762a5648ff7ef1b7
Aedif <vaitkus.martynas@gmail.com>
2022-11-01 01:07:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+![GitHub Latest Version](https://img.shields.io/github/v/release/Aedif/move-that-for-you?sort=semver)
+![GitHub Latest Release](https://img.shields.io/github/downloads/Aedif/move-that-for-you/latest/move-that-for-you.zip)
+![GitHub All Releases](https://img.shields.io/github/downloads/Aedif/move-that-for-you/move-that-for-you.zip)
+
+# Move That for you
+
+Foundry VTT module for allowing players to move tiles.
+
+## Usage
---
+++ move-that-for-you.js
@@ -0,0 +1,107 @@
+const MODULE_ID = "move-that-for-you";
+
+Hooks.once("init", () => {
+  game.socket?.on(`module.${MODULE_ID}`, (message) => {
+    if (game.user.isGM && message.handlerName === "tile" && message.type === "UPDATE") {
+      const isResponsibleGM = !game.users
+        .filter((user) => user.isGM && (user.active || user.isActive))
+        .some((other) => other.id < game.user.id);
+      if (!isResponsibleGM) return;
+      canvas.scene.updateEmbeddedDocuments("Tile", [message.args.data], message.args.options);
+    }
+  });
+
+  ["_canDrag", "_canHover", "_canControl"].forEach((method) => {
+    libWrapper.register(
+      MODULE_ID,
+      `Tile.prototype.${method}`,
+      function (wrapped, ...args) {
+        let result = wrapped(...args);
+        if (game.user.isGm) return result;
+        return !game.paused && this.document.getFlag(MODULE_ID, "allowPlayerMove");
+      },
+      "WRAPPER"
+    );
+  });
+
+  libWrapper.register(
+    MODULE_ID,
+    `Tile.prototype._canHUD`,
+    function (wrapped, ...args) {
+      let result = wrapped(...args);
+      if (game.user.isGM) return result;
+      return false;
+    },
+    "WRAPPER"
+  );
+});
+
+Hooks.on("getSceneControlButtons", (controls) => {
+  if (game.user.isGM) return;
+
+  for (let i = 0; i < controls.length; i++) {
+    if (controls[i].name === "tiles") {
+      controls[i].visible = true;
+
+      // Remove core tools, only keeping "select";
+      const tools = [];
+      const coreTools = ["tile", "browse", "foreground"];
+      controls[i].tools.forEach((t) => {
+        if (!coreTools.includes(t.name)) {
+          tools.push(t);
+        }
+      });
+
+      controls[i].tools = tools;
+
+      return;
+    }
+  }
+});
+
+Hooks.once("canvasReady", () => {
+  if (!game.user.isGM) {
+    Hooks.on("preUpdateTile", (document, data, options, userId) => {
+      if (game.user.id === userId) {
+        // Only allow positional updates
+        let keyNum = Object.keys(data).length;
+        if ("x" in data) keyNum--;
+        if ("y" in data) keyNum--;
+
+        if (keyNum === 1) {
+          const message = {
+            handlerName: "tile",
+            args: { document, data, options },
+            type: "UPDATE",
+          };
+          game.socket?.emit(`module.${MODULE_ID}`, message);
+          return false;
+        }
+      }
+    });
+  }
+});
+
+Hooks.on("renderTileHUD", (hud, form, options) => {
+  const playerMoveControl = $(`
+  <div class="control-icon " data-action="playerMove">
+    <i title="${game.i18n.format("move-that-for-you.control-title")}" class="fas fa-people-carry"></i>
+  </div>`);
+  form.find("div.col.right").last().append(playerMoveControl);
+
+  const doc = hud.object.document;
+
+  if (doc.getFlag(MODULE_ID, "allowPlayerMove")) {
+    playerMoveControl.addClass("active");
+  }
+
+  playerMoveControl.click(async () => {
+    if (playerMoveControl.hasClass("active")) {
+      await doc.unsetFlag(MODULE_ID, "allowPlayerMove");
+      playerMoveControl.removeClass("active");
+    } else {
+      doc.setFlag(MODULE_ID, "allowPlayerMove", true);
+      playerMoveControl.addClass("active");
+    }
+  });
+});
